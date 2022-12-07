5ae19bd392aa23eef74d4b7b91ef16f4f1d54358
enrico <enrico.dsf@gmail.com>
2022-12-07 02:20:42 +0100

    optimized day 1

--- day01.py
+++ day01.py
@@ -1,3 +1,5 @@
+from time import time
+
 with open(__file__.replace(".py", "_data")) as f:
     data = f.read()
 
@@ -5,4 +7,9 @@ with open(__file__.replace(".py", "_data")) as f:
 print(sum(1 if c == '(' else -1 for c in data))
 
 # PART 2
-print(next(i for i in range(len(data)) if sum(1 if c == '(' else -1 for c in data[:i]) == -1))
+count = 0
+for i, c in enumerate(data, 1):
+    count += 1 if c == "(" else -1
+    if count == -1:
+        print(i)
+        break
